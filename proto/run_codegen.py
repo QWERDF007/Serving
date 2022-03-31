3908f9a10d33fb639414a6c8458264fde5e05030
wangtao <595517713@qq.com>
2022-03-31 22:39:36 +0800

    feat: 添加模块 proto，使用 protocol buffers 定义 PipelineService，生成 python 文件

---
+++ proto/run_codegen.py
@@ -0,0 +1,10 @@
+"""Runs protoc with the gRPC plugin to generate messages and gRPC stubs."""
+
+from grpc_tools import protoc
+
+protoc.main((
+    '',
+    '-I.',
+    '--python_out=.',
+    '--grpc_python_out=.',
+    'pipeline_service.proto',))
