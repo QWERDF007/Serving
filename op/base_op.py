5be323929a36bc294956b99b0c79d91b6977615a
wangtao <595517713@qq.com>
2022-04-05 22:36:26 +0800

    feat: 添加基础op，ClsOp、DetOp、SegOp

---
+++ op/base_op.py
@@ -0,0 +1,153 @@
+import os
+from op.operator import Op
+from utils.logger import get_logger
+
+_LOGGER = get_logger("Serving")
+
+
+class ClsOp(Op):
+    def init_op(self):
+        pass
+
+    def preprocess(self, input_dicts, data_id=0, log_id=0):
+        """
+                预处理，为 process 装配数据。用户可以重载本函数
+
+                Args:
+                    input_dicts: 要被预处理的数据
+                    data_id: 内部唯一 id，自增
+                    log_id: 全局唯一 id for RTT，默认 0
+
+                Return:
+                    output_data: 给 process 的数据
+                    is_skip_process: 是否跳过 process，默认 False
+                    prod_errcode: 默认 None，否则发生业务错误。处理方式和异常一样
+                    prod_errinfo: 默认 ""
+                """
+        # multiple previous Op
+        if len(input_dicts) != 1:
+            _LOGGER.critical(self._log(
+                "Failed to run preprocess: this Op has multiple previous inputs. Please override this func."))
+            os._exit(-1)
+
+        (_, input_dict), = input_dicts.items()
+        return input_dict, False, None, ""
+
+    def postprocess(self, input_data, fetch_data, data_id=0, log_id=0):
+        """
+                postprocess 步骤，汇聚数据给下一 Op 或输出
+
+                Args:
+                    input_data: preprocess 步骤返回的数据，dict (单预测) 或 list (批量预测)
+                    fetch_data: process 步骤返回的数据，dict (单预测) 或 list (批量预测)
+                    data_id: 内部唯一 id，自增
+                    log_id: log_id，默认 0
+
+                Returns:
+                    fetch_dict: dict 类型结果
+                    prod_errcode: 默认 None, 否则, 业务错误发生. 它与异常处理方式一样
+                    prod_errinfo: 默认 ""
+                """
+        fetch_dict = {}
+        if isinstance(fetch_data, dict):
+            fetch_dict = fetch_data
+        return fetch_dict, None, ""
+
+
+
+class SegOp(Op):
+    def init_op(self):
+        pass
+
+    def preprocess(self, input_dicts, data_id=0, log_id=0):
+        """
+                预处理，为 process 装配数据。用户可以重载本函数
+
+                Args:
+                    input_dicts: 要被预处理的数据
+                    data_id: 内部唯一 id，自增
+                    log_id: 全局唯一 id for RTT，默认 0
+
+                Return:
+                    output_data: 给 process 的数据
+                    is_skip_process: 是否跳过 process，默认 False
+                    prod_errcode: 默认 None，否则发生业务错误。处理方式和异常一样
+                    prod_errinfo: 默认 ""
+                """
+        # multiple previous Op
+        if len(input_dicts) != 1:
+            _LOGGER.critical(self._log(
+                "Failed to run preprocess: this Op has multiple previous inputs. Please override this func."))
+            os._exit(-1)
+
+        (_, input_dict), = input_dicts.items()
+        return input_dict, False, None, ""
+
+    def postprocess(self, input_data, fetch_data, data_id=0, log_id=0):
+        """
+                postprocess 步骤，汇聚数据给下一 Op 或输出
+
+                Args:
+                    input_data: preprocess 步骤返回的数据，dict (单预测) 或 list (批量预测)
+                    fetch_data: process 步骤返回的数据，dict (单预测) 或 list (批量预测)
+                    data_id: 内部唯一 id，自增
+                    log_id: log_id，默认 0
+
+                Returns:
+                    fetch_dict: dict 类型结果
+                    prod_errcode: 默认 None, 否则, 业务错误发生. 它与异常处理方式一样
+                    prod_errinfo: 默认 ""
+                """
+        fetch_dict = {}
+        if isinstance(fetch_data, dict):
+            fetch_dict = fetch_data
+        return fetch_dict, None, ""
+
+
+class DetOp(Op):
+    def init_op(self):
+        pass
+
+    def preprocess(self, input_dicts, data_id=0, log_id=0):
+        """
+                预处理，为 process 装配数据。用户可以重载本函数
+
+                Args:
+                    input_dicts: 要被预处理的数据
+                    data_id: 内部唯一 id，自增
+                    log_id: 全局唯一 id for RTT，默认 0
+
+                Return:
+                    output_data: 给 process 的数据
+                    is_skip_process: 是否跳过 process，默认 False
+                    prod_errcode: 默认 None，否则发生业务错误。处理方式和异常一样
+                    prod_errinfo: 默认 ""
+                """
+        # multiple previous Op
+        if len(input_dicts) != 1:
+            _LOGGER.critical(self._log(
+                "Failed to run preprocess: this Op has multiple previous inputs. Please override this func."))
+            os._exit(-1)
+
+        (_, input_dict), = input_dicts.items()
+        return input_dict, False, None, ""
+
+    def postprocess(self, input_data, fetch_data, data_id=0, log_id=0):
+        """
+                postprocess 步骤，汇聚数据给下一 Op 或输出
+
+                Args:
+                    input_data: preprocess 步骤返回的数据，dict (单预测) 或 list (批量预测)
+                    fetch_data: process 步骤返回的数据，dict (单预测) 或 list (批量预测)
+                    data_id: 内部唯一 id，自增
+                    log_id: log_id，默认 0
+
+                Returns:
+                    fetch_dict: dict 类型结果
+                    prod_errcode: 默认 None, 否则, 业务错误发生. 它与异常处理方式一样
+                    prod_errinfo: 默认 ""
+                """
+        fetch_dict = {}
+        if isinstance(fetch_data, dict):
+            fetch_dict = fetch_data
+        return fetch_dict, None, ""
