f502014eb21e126b32c01c0436024b88515ab09c
wangtao <595517713@qq.com>
2022-04-07 17:51:37 +0800

    feat: 添加 torch 分类模型创建、加载、transforms 等

--- inference/config.py
+++ inference/config.py
@@ -8,26 +8,82 @@ _LOGGER = get_logger("Serving")
 class Config(object):
     def __init__(self, yml_file):
         with io.open(yml_file, encoding='utf-8') as f:
-            self._conf = yaml.load(f.read(), yaml.FullLoader)
+            model_conf = yaml.load(f.read(), yaml.FullLoader)
 
-        Config.check_model_conf(self._conf)
+        Config.check_model_conf(model_conf)
+        self._input_names = model_conf['input_names']
+        self._output_names = model_conf['output_names']
+        self._model_name = model_conf['model_name']
+        self._model_type = model_conf['model_type']
+        self._checkpoint_path = model_conf['checkpoint_path']
+        self._num_classes = model_conf['num_classes']
+        self._mean = model_conf['mean']
+        self._std = model_conf['std']
+        self._use_gpu = False
+        self._device = 'cpu'
+
+    @property
+    def model_name(self):
+        return self._model_name
+
+    @property
+    def model_type(self):
+        return self._model_type
+
+    @property
+    def checkpoint_path(self):
+        return self._checkpoint_path
+
+    @property
+    def num_classes(self):
+        return self._num_classes
+
+    @property
+    def mean(self):
+        return self._mean
+
+    @property
+    def std(self):
+        return self._std
+
+    @property
+    def device(self):
+        return self._device
+
+    @property
+    def use_gpu(self):
+        return self._use_gpu
+
+    @property
+    def input_names(self):
+        return self._input_names
+
+    @property
+    def output_names(self):
+        return self._output_names
+
+    def disable_gpu(self):
+        self._use_gpu = True
+        self._device = 'cpu'
+
+    def enable_use_gpu(self, gpu_id):
+        self._use_gpu = True
+        self._device = 'cuda:{}'.format(gpu_id)
 
     @staticmethod
     def check_model_conf(conf):
         default_conf = {
-            "use_gpu": False,
-            "gpu_id": -1,
         }
 
         conf_type = {
-            "use_gpu": bool,
-            "gpu_id": int,
+            "model_name": str,
+            "model_type": str,
             "checkpoint_path": str,
             "num_classes": int,
         }
 
         conf_qualification = {
-            "gpu_id": [(">=", 0), ("<=", 65536)],
+            "model_type": [("in", ["rec", "seg", "det"])],
             "num_classes": [(">=", 1), ("<=", 65536)],
         }
 
--- inference/predictor.py
+++ inference/predictor.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 import timm
 import torch
+from torchvision import transforms
 
 from utils.logger import get_logger
 
@@ -11,14 +12,16 @@ _LOGGER = get_logger("Serving")
 class Predictor(ABC):
     def __init__(self, config):
         self._config = config
+        self._transforms = None
         self._predictor = None
-        self._input_handle = None
-        self._output_handle = None
+        self._input_handle = {name: None for name in self._config.input_names}
+        self._output_handle = {name: None for name in self._config.output_names}
+        self._device = torch.device(self._config.device)
+        self._init_predictor()
 
     def _init_predictor(self):
         self._create_predictor()
-        if self._config.use_gpu:
-            self._predictor.cuda(self._config.gpuid)
+        self._predictor.to(self._device)
         self._predictor.eval()
 
     @abstractmethod
@@ -33,21 +36,42 @@ class Predictor(ABC):
     def input_handle(self):
         return self._input_handle
 
-    @input_handle.setter
-    def input_handle(self, tensor):
-        if tensor.device != self._predictor.device:
-            self._input_handle = tensor.to(self._predictor.device)
+    def set_input_handle(self, name, ndarray):
+        tensor = torch.from_numpy(ndarray).contiguous()
+        if self._transforms is not None:
+            tensor = self._transforms(tensor)
+        if tensor.device != self._device:
+            self._input_handle[name] = tensor.to(self._device)
         else:
-            self._input_handle = tensor
+            self._input_handle[name] = tensor
 
     @property
     def output_handle(self):
         return self._output_handle
 
+    @property
+    def device(self):
+        return self._device
+
+    def get_input_names(self):
+        return self._config.input_names
+
+    def get_input_handle(self, name):
+        return self._input_handle[name]
+
+    def get_output_names(self):
+        return self._config.output_names
+
+    def get_output_handle(self, name):
+        return self._output_handle[name]
+
 
 class Classifier(Predictor):
     def __init__(self, config):
         super(Classifier, self).__init__(config)
+        self._transforms = transforms.Compose([
+            transforms.Normalize(mean=self._config.mean, std=self._config.std),
+        ])
 
     def _create_predictor(self):
         self._predictor = timm.create_model(
@@ -59,11 +83,14 @@ class Classifier(Predictor):
 
     @torch.no_grad()
     def run(self):
-        self._output_handle = self._predictor(self._input_handle)
+        self._output_handle[self._config.output_names[0]] = \
+            self._predictor(self._input_handle[self._config.input_names[0]])
 
 
 def create_predictor(config):
     if config.model_type == 'rec':
-        return Classifier(config)
+        classifier = Classifier(config)
+        _LOGGER.info("create a Classifier: {} on {}".format(classifier, classifier.device))
+        return classifier
     else:
         return NotImplementedError("no such model_type={}".format(config.model_type))
